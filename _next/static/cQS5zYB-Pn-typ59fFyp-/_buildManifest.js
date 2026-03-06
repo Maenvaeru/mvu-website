4945c05e9567ef6834379e108108a52542b3f12a
MaenVaerU <>
2026-03-06 13:01:08 +0300

    Updates

--- _next/static/cQS5zYB-Pn-typ59fFyp-/_buildManifest.js
+++ _next/static/cQS5zYB-Pn-typ59fFyp-/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/mvu-website//_next/:path+",
-        "destination": "/mvu-website/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
